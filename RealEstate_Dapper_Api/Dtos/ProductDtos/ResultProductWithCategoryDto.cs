8d511b2034a6b5ffe4f1dd6ee0ff7ba35408a6a5
ahmetyvs <afisci@gmail.com>
2023-08-22 15:23:38 +0300

    22.08.2023-Ders-13

--- RealEstate_Dapper_Api/Dtos/ProductDtos/ResultProductWithCategoryDto.cs
+++ RealEstate_Dapper_Api/Dtos/ProductDtos/ResultProductWithCategoryDto.cs
@@ -8,5 +8,6 @@
         public string City { get; set; }
         public string District { get; set; }
         public string CategoryName { get; set; }
+        public string Address { get; set; }
     }
 }
