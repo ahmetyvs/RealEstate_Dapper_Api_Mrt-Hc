ab02784f3598648a61a90da966a572aa30d6878e
ahmetyvs <afisci@gmail.com>
2023-08-28 14:28:16 +0300

    28.08.2023-17.Ders

--- RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultWhoWeAreComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultWhoWeAreComponentPartial.cs
@@ -17,15 +17,19 @@ namespace RealEstate_Dapper_UI.ViewComponents.HomePage
         {
             var client = _httpClientFactory.CreateClient();
             var responsMessage = await client.GetAsync("https://localhost:7067/api/WhoWeAreDetail");
+            var responsData = await client.GetAsync("https://localhost:7067/api/Service");
             if (responsMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responsMessage.Content.ReadAsStringAsync();
                 var value = JsonConvert.DeserializeObject<List<ResultWhoWeAreDetailDto>>(jsonData);
+                var jsonData2 = await responsData.Content.ReadAsStringAsync();
+
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData2);
                 ViewBag.title=value.Select(x=>x.Title).FirstOrDefault();
                 ViewBag.subTitle=value.Select(x=>x.SubTitle).FirstOrDefault();
                 ViewBag.description1=value.Select(x=>x.Description1).FirstOrDefault();
                 ViewBag.description2 = value.Select(x=>x.Description2).FirstOrDefault();
-                return View();
+                return View(values);
             }
             return View();
         }
---
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultWhoweAreServiceComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.ServiceDtos;
+
+namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+{
+    public class _DefaultWhoweAreServiceComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultWhoweAreServiceComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsData = await client.GetAsync("https://localhost:7067/api/Service");
+            if (responsData.IsSuccessStatusCode)
+            {
+                var jsonData = await responsData.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
