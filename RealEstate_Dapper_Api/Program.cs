39ee849ba08cff2e10010baa82650ea06e78d4e8
ahmetyvs <afisci@gmail.com>
2023-09-18 17:44:04 +0300

    18.09.2023-21.Ders

--- RealEstate_Dapper_Api/Program.cs
+++ RealEstate_Dapper_Api/Program.cs
@@ -5,6 +5,7 @@ using RealEstate_Dapper_Api.Repositories.PopularLocationRepository;
 using RealEstate_Dapper_Api.Repositories.ProductDetailRepository;
 using RealEstate_Dapper_Api.Repositories.ProductRepository;
 using RealEstate_Dapper_Api.Repositories.ServiceRepository;
+using RealEstate_Dapper_Api.Repositories.TestimonialRepository;
 using RealEstate_Dapper_Api.Repositories.WhoWeAreRepository;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +21,7 @@ builder.Services.AddTransient<IWhoWeAreDetailRepository, WhoWeAreDetailRepositor
 builder.Services.AddTransient<IServiceRepository, ServiceRepository>();
 builder.Services.AddTransient<IBottomGridRepository, BottomGridRepository>(); 
 builder.Services.AddTransient<IPopularLocationRepository, PopularLocationRepository>(); 
+builder.Services.AddTransient<ITestimonialRepository, TestimonialRepository>();
 
 
 builder.Services.AddControllers();
---
+++ RealEstate_Dapper_Api/Repositories/TestimonialRepository/TestimonialRepository.cs
@@ -0,0 +1,77 @@
+﻿using RealEstate_Dapper_Api.Models.DapperContext;
+using Dapper;
+using RealEstate_Dapper_Api.Dtos.TestimonialDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.TestimonialRepository
+{
+    public class TestimonialRepository : ITestimonialRepository
+    {
+        private readonly Context _context;
+        public TestimonialRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async void CreateTestimonial(CreateTestimonialDto createTestimonilDto)
+        {
+            string query = "insert into Testimonial (NameSurname,Title,Comment,Status) values (@nameSurname,@title,@comment,@status)";
+            var parameters = new DynamicParameters();
+            parameters.Add("@nameSurname", createTestimonilDto.NameSurname);
+            parameters.Add("@title", createTestimonilDto.Title);
+            parameters.Add("@comment", createTestimonilDto.Comment);
+            parameters.Add("@tatus", true);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async void DeleteTestimonial(int id)
+        {
+            string query = "Delete From Testimonial Where TestimonialID=@testimonialID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@testimonialID", id);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async Task<List<ResultTestimonialDto>> GetAllTestimonialAsync()
+        {
+            string query = "Select * From Testimonial";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultTestimonialDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public async Task<GetByIDTestimonialDto> GetTestimonial(int id)
+        {
+            string query = "Select * From Testimonial Where TestimonialID=@testimonialID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@testimonialID", id);
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryFirstOrDefaultAsync<GetByIDTestimonialDto>(query, parameters);
+                return values;
+            }
+        }
+
+        public async void UpdateTestimonial(UpdateTestimonialDto categoryDto)
+        {
+            string query = "Update Testimonial Set NameSurname=@nameSurname,Title=@title,Comment=@comment,Status=@status where TestimonialID=@testimonialID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@nameSurname", categoryDto.NameSurname);
+            parameters.Add("@title", categoryDto.Title);
+            parameters.Add("@comment", categoryDto.Comment);
+            parameters.Add("@status", categoryDto.Status);
+            parameters.Add("@testimonialID", categoryDto.TestimonialID);
+            using (var connectiont = _context.CreateConnection())
+            {
+                await connectiont.ExecuteAsync(query, parameters);
+            }
+        }
+    }
+}
